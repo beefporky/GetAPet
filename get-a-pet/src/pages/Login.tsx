8faaae9d829d2f1bddbdcfd0dfefeb711fb628ce
James B <perpetuus.flamma@gmail.com>
2024-04-22 15:03:08 +1000

    Update login component to use Suspense and Await

--- get-a-pet/src/pages/Login.tsx
+++ get-a-pet/src/pages/Login.tsx
@@ -1,8 +1,8 @@
-import { defer, redirect, useLoaderData, useNavigate } from "react-router-dom"
+import { Await, defer, redirect, useLoaderData, useNavigate } from "react-router-dom"
 import { TokenType, authenticate } from "../services/auth"
 import { isTokenValid } from "../utils/auth"
 import { useAuth } from "../store/auth-context"
-import { useEffect } from "react"
+import { Suspense } from "react"
 import Loading from "../components/ui/Loading/Loading"
 import { Request } from "../utils/network"
 
@@ -21,17 +21,15 @@ const Login = () => {
         navigate(prevPath || '/', { replace: true });
     }
 
-    useEffect(() => {
-        if (token) {
-            token.then((resolvedToken: TokenType) => {
-                setBearerToken(resolvedToken);
+    return <Suspense fallback={<Loading />}>
+        <Await resolve={token}>
+            {(tokenData) => {
+                setBearerToken(tokenData);
                 redirectToPreviousPath();
-            });
-        }
-    }, [token]);
-    return (
-        <Loading />
-    )
+                return <></>
+            }}
+        </Await>
+    </Suspense>
 }
 
 export default Login
